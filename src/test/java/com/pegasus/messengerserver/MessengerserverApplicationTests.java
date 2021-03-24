446fc5ec00a7a0bb472c5198c2a1ef7a0a63ac60
miracle <x.firedragon@gmail.com>
2021-03-24 19:02:42 +0500

    Init messenger server with keycloak provider

---
+++ src/test/java/com/pegasus/messengerserver/MessengerserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pegasus.messengerserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessengerserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
