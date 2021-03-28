818b86f820b735393d0d60cd917ff2318ec0ed8a
miracle <x.firedragon@gmail.com>
2021-03-28 20:07:21 +0500

    fix/messages-schema

--- src/main/java/com/pegasus/messengerserver/controller/MessageController.java
+++ src/main/java/com/pegasus/messengerserver/controller/MessageController.java
@@ -4,13 +4,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @RestController
 @RequestMapping("message")
 public class MessageController {
 
   @GetMapping
-  public String getAll() {
-    return "All messages";
+  public List<Map<String, String>> getAll() {
+    return new ArrayList<Map<String, String>>() {{
+      add(new HashMap<String, String>() {{
+        put("id", "1");
+        put("text", "First text");
+      }});
+      add(new HashMap<String, String>() {{
+        put("id", "2");
+        put("text", "Second text");
+      }});
+    }};
   }
 
 }
